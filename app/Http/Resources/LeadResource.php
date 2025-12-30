ee3b9253f680e198e0ad1a9035d0e8886caa9a9a
mathfajardo <mathfajardo.contato@hotmail.com>
2025-12-30 18:07:57 -0300

    fix: ajustando data e hora do resources de lead

--- app/Http/Resources/LeadResource.php
+++ app/Http/Resources/LeadResource.php
@@ -23,7 +23,7 @@ class LeadResource extends JsonResource
             "status" => $this->status,
             "observacoes" => $this->observacoes,
             "created_at" => Carbon::parse($this->created_at)->format('d/m/Y H:i:s'),
-            "updated_at" => Carbon::parse($this->updated_at)->format('d/m/Y H:i:s')
+            "updated_at" => Carbon::parse($this->updated_at)->format('d/m H:i')//Carbon::parse($this->updated_at)->format('d/m/Y H:i:s')
         ];
     }
 }
